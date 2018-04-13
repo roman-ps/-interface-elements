1ba21d994bf65c8ca83da882a883fc8384479fbb
Pozharov Roman <ru.roman.ps@gmail.com>
2018-04-13 06:46:20 +0400

    Добавлен слайдер-карусель

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function hideSliders() {
-  var images = document.querySelectorAll('.slider');
+  var images = document.querySelectorAll('.slider2');
   for (var i = 0; i < images.length; ++i) {
     images[i].style.display = 'none';
   }
@@ -22,3 +22,30 @@ function slider3() {
   var slide3 = document.querySelector(".slider--third2");
   slide3.style.display = "flex";
 }
+
+var img = document.querySelectorAll(".slider-img");
+var prev = document.querySelector(".btn--prev");
+var next = document.querySelector(".btn--next");
+var i = 0;
+
+next.addEventListener("click", function(event) {
+  event.preventDefault();
+  img[i].classList.add("slider-img__hide");
+  i++;
+  if (i >= img.lenth) {
+    i = 0;
+//    img[i].classList.remove("slider-img__show");
+  }
+  img[i].classList.add("slider-img__show");
+})
+
+prev.addEventListener("click", function(event) {
+  event.preventDefault();
+  img[i].classList.add("slider-img__hide");
+  i--;
+  if (i < 0) {
+    i = img.length-1;
+//    img[i].classList.remove("slider-img__show");
+  }
+  img[i].classList.add("slider-img__show");
+})
