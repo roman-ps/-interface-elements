fa7eac9090bc483c91f363a8bc179e9ad7c975d5
Pozharov Roman <ru.roman.ps@gmail.com>
2018-04-16 16:12:05 +0400

    Добавлено подменю 2 уровня

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,7 @@
 var cat1 = document.querySelector(".menu__item-cat1");
 var submenu = document.querySelector(".menu__submenu");
+var subItem = document.querySelector(".menu__item-submenu--sub");
+var submenu2 = document.querySelector(".menu__submenu2");
 
 cat1.addEventListener("mouseover", function(event) {
   event.preventDefault();
@@ -9,4 +11,14 @@ cat1.addEventListener("mouseover", function(event) {
 cat1.addEventListener("mouseout", function(event) {
   event.preventDefault();
   submenu.classList.remove("menu__submenu-show");
+})
+
+subItem.addEventListener("mouseover", function(event) {
+  event.preventDefault();
+  submenu2.classList.add("menu__submenu-show");
+})
+
+subItem.addEventListener("mouseout", function(event) {
+  event.preventDefault();
+  submenu2.classList.remove("menu__submenu-show");
 })
