a568377ff4e7c1cad8b6fc94c1488b02e495d580
Pozharov Roman <ru.roman.ps@gmail.com>
2018-04-17 05:43:05 +0400

    Убрано подменю 2 уровня + правки

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,12 @@
-var cat1 = document.querySelector(".menu__item-cat1");
+var itemOpenSubmenu = document.querySelector(".menu__item--submenu");
 var submenu = document.querySelector(".menu__submenu");
-var subItem = document.querySelector(".menu__item-submenu--sub");
-var submenu2 = document.querySelector(".menu__submenu2");
 
-cat1.addEventListener("mouseover", function(event) {
+itemOpenSubmenu.addEventListener("mouseover", function(event) {
   event.preventDefault();
   submenu.classList.add("menu__submenu-show");
 })
 
-cat1.addEventListener("mouseout", function(event) {
+itemOpenSubmenu.addEventListener("mouseout", function(event) {
   event.preventDefault();
   submenu.classList.remove("menu__submenu-show");
-})
-
-subItem.addEventListener("mouseover", function(event) {
-  event.preventDefault();
-  submenu2.classList.add("menu__submenu-show");
-})
-
-subItem.addEventListener("mouseout", function(event) {
-  event.preventDefault();
-  submenu2.classList.remove("menu__submenu-show");
 })
