cc42809eadac3681ed240ca7290cd6bba7749902
Roman <ru.roman.ps@gmail.com>
2020-02-18 08:29:17 +0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -13,19 +13,3 @@ OpenSubmenu.addEventListener("mouseout", function(event) {
   event.preventDefault();
   submenu.classList.remove("submenu--show");
 });
-
-//var OpenSubmenu = document.querySelectorAll(".menu__item--submenu");
-//var submenu = document.querySelectorAll(".submenu");
-//var i;
-//
-//function opens() {
-//  for (i = 0; i < OpenSubmenu.length; i++) {
-//  submenu[i].style.display = "flex";
-//  }
-//}
-//
-//function closes() {
-//  for (i = 0; i < OpenSubmenu.length; i++) {
-//  submenu[i].style.display = "none";
-//  }
-//}
